a3fd6f8134b2521601907c947c615400a030169f
zwd-admin <810095178@qq.com>
2019-02-20 08:42:45 +0800

    feign

---
+++ spring-cloud/spring-cloud-feign/src/main/java/com/example/zwd/springcloudfeign/SpringCloudFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.zwd.springcloudfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFeignApplication.class, args);
+	}
+
+}
---
+++ spring-cloud/spring-cloud-feign/src/test/java/com/example/zwd/springcloudfeign/SpringCloudFeignApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.zwd.springcloudfeign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudFeignApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
